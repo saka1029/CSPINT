8e203fb763317a9b8b893d1efec7b20f1c6ed4d8
saka1029 <saka1029@gmail.com>
2020-03-31 06:01:56 +0900

    単体テスト拡充

---
+++ src/test/java/test/jp/saka1029/cspint/sequential/TestConstraint.java
@@ -0,0 +1,44 @@
+package test.jp.saka1029.cspint.sequential;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import jp.saka1029.cspint.sequential.Constraint;
+import jp.saka1029.cspint.sequential.Domain;
+import jp.saka1029.cspint.sequential.Problem;
+import jp.saka1029.cspint.sequential.Variable;
+
+class TestConstraint {
+
+    @Test
+    void testVariables() {
+        Problem p = new Problem();
+        Variable v = p.variable("v", Domain.of(1, 2));
+        Constraint c = p.constraint(x -> x == 1, v);
+        assertEquals(List.of(v), c.variables);
+    }
+
+    @Test
+    void testPredicate() {
+        Problem p = new Problem();
+        Variable v = p.variable("v", Domain.of(1, 2));
+        Constraint c = p.constraint(x -> x == 1, v);
+        assertEquals(true, c.predicate.test(1));
+    }
+
+    @Test
+    void testVariablesUnmodifiable() {
+        Problem p = new Problem();
+        Variable v = p.variable("v", Domain.of(1, 2));
+        Constraint c = p.constraint(x -> x == 1, v);
+        try {
+            c.variables.set(0, null);
+            fail();
+        } catch (UnsupportedOperationException e) {
+        }
+    }
+
+}
