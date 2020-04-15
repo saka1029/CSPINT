214bd81760ac16cea1b2a5d97e011ffbbba7df8e
saka1029 <saka1029@gmail.com>
2020-04-15 15:32:47 +0900

    dependナンバーリンク追加

---
+++ src/test/java/test/jp/saka1029/cspint/depend/puzzle/Testナンバーリンク.java
@@ -0,0 +1,14 @@
+package test.jp.saka1029.cspint.depend.puzzle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Testナンバーリンク {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
