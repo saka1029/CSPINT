83cd4cce3eb59eb0b260e643975f2a709aa92bf9
saka1029 <saka1029@gmail.com>
2020-06-08 08:11:09 +0900

    aa

--- src/test/java/test/jp/saka1029/cspint/puzzle/Testナップサック.java
+++ src/test/java/test/jp/saka1029/cspint/puzzle/Testナップサック.java
@@ -23,7 +23,8 @@ class Testナップサック {
             .mapToObj(i -> p.variable("v" + i, Domain.rangeClosed(0, maxSize / sizes[i])))
             .toArray(Variable[]::new);
         // 全体の単一制約
-        // p.constraint(q -> IntStream.range(0, size).map(i -> q[i] * sizes[i]).sum() <= maxSize, variables);
+        // p.constraint(q -> IntStream.range(0, size).map(i -> q[i] *
+        // sizes[i]).sum() <= maxSize, variables);
         // 分割した制約
         IntStream.range(0, size)
             .forEach(i -> p.constraint(
@@ -39,11 +40,23 @@ class Testナップサック {
     }
 
     /**
-     * 例題
-     * 容量65のナップサックに次の表にある品物を詰め込むことにします．この時，詰め込んだ品物の総価値を最大にするためには何をいくつ詰め込むと良いでしょうか．ただし，同じ品物を何個詰め込んでも良いものとします．
+     * 2.5 ナップサック問題
+     * https://www.msi.co.jp/nuopt/docs/v19/examples/html/02-05-00.html
      *
-     * 品物 1個あたりの価値 1個あたりのサイズ 缶コーヒー 120 10 水入りペットボトル 130 12 バナナ 80 7 りんご 100 9
-     * おにぎり 250 21 パン 185 16
+     * 例題
+     * 容量65のナップサックに次の表にある品物を詰め込むことにします．
+     * この時，詰め込んだ品物の総価値を最大にするためには何をいくつ詰め込むと良いでしょうか．
+     * ただし，同じ品物を何個詰め込んでも良いものとします．
+     *
+     * <pre>
+     * 品物 1個あたりの価値 1個あたりのサイズ
+     * 缶コーヒー 120 10
+     * 水入りペットボトル 130 12
+     * バナナ 80 7 りんご 100 9
+     * おにぎり 250 21
+     * パン 185 16
+     * </pre>
+     *
      */
     @Test
     void test() {
